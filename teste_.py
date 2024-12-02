b1447e770e7279e82c7b0559f4c6e2add5ebed10
prof-Jefferson <jefferson.oliveira@ifpr.edu.br>
2024-12-01 22:09:12 -0300

    bird: jump

--- teste_.py
+++ teste_.py
@@ -22,6 +22,7 @@ while running:
     # Exibir o primeiro frame carregado na tela
     screen.fill((135, 206, 250))  # Fundo azul claro
     screen.blit(bird.frames[0], (bird.x, bird.y))  # Desenha o primeiro frame
-    pygame.display.update()
+    #ird.jump()
+    pygame.display.update()    
 
 pygame.quit()
